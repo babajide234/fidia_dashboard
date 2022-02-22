9b2cdb2841defccd68099a3c928a3e7304a05b42
Babajide Tomoshegbo <babajidetomoshegbo@yahoo.com>
2022-02-22 19:09:03 +0100

    first comiit

---
+++ components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav class="">
+        <div class="">
+
+            <div class="logo"></div>
+            <div class="navigation">
+                <ul class="">
+                    <li>Home</li>
+                    <li><a href="">Dashboard</a></li>
+                    <li><a href="">Projects</a></li>
+                    <li><a href="">Tasks</a></li>
+                    <li><a href="">Reporting</a></li>
+                    <li><a href="">Users</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
